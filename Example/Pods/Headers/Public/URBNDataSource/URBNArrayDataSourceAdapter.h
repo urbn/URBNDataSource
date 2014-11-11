8ac125d55e696d0e3331fcc39c492b835af030c9
jgrandelli <jasongrandelli@gmail.com>
2014-11-11 09:51:21 -0500

    pods deleted some shit in example

--- Example/Pods/Headers/Public/URBNDataSource/URBNArrayDataSourceAdapter.h
+++
@@ -1,1 +0,0 @@
-../../../URBNDataSource/Pod/Classes/URBNArrayDataSourceAdapter.h
--- Example/Pods/Target Support Files/Pods-Tests-URBNDataSource/Pods-Tests-URBNDataSource-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Tests_URBNDataSource : NSObject
-@end
-@implementation PodsDummy_Pods_Tests_URBNDataSource
-@end
