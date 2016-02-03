605d5a5eb7eb53cdc27a38b8f20f037301a33dc5
Nick DiStefano <NDistefano@urbn.com>
2016-02-02 20:08:59 -0500

    fix test target

--- Example/Pods/Target Support Files/Pods-ExampleTests/Pods-ExampleTests-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-ExampleTests/Pods-ExampleTests-frameworks.sh
@@ -84,8 +84,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "Pods-Tests/URBNDataSource.framework"
+  install_framework "Pods-ExampleTests/URBNDataSource.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "Pods-Tests/URBNDataSource.framework"
+  install_framework "Pods-ExampleTests/URBNDataSource.framework"
 fi
